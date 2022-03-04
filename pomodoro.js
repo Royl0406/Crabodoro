ebc9d17ece0ef17fd3f41e1989024ec93a3c41b7
Roy Liu <roy@Roys-MacBook-Pro.local>
2022-03-03 20:11:50 -0800

    working timer, adding coin in progress

--- pomodoro.js
+++ pomodoro.js
@@ -1,20 +1,53 @@
+let isOnTask = () => {
+    //isDistractingPage(fetchWebpage);
+    //TODO: write the function
+    return true;
+}
+
+let fetchWebpage = () => {
+
+}
+
+let isDistractingPage = () => {
+
+}
+
+let calculateCoinEarned = (elapsed, rate) => {
+    return elapsed * rate;
+}
 window.onload = () => {
-    let div = document.getElementById("timer");
+    let timerDiv = document.getElementById("timer");
+    let coinDiv = document.getElementById("coin-count");
 
     const MILLISECONDS_PER_SECOND = 1000;
     const SECONDS_PER_MINUTE = 60;
     const SESSION_TIME_MINUTES = 40;
+    const MAX_COIN = 200;
+    const TOTAL_TIME_MS = SESSION_TIME_MINUTES * SECONDS_PER_MINUTE * MILLISECONDS_PER_SECOND;
+    const COIN_RATE = MAX_COIN / TOTAL_TIME_MS;
 
     // Current time in milliseconds (from the epoch)
     let startTime = (new Date()).getTime();
-    let remainingTime = SESSION_TIME_MINUTES * SECONDS_PER_MINUTE * MILLISECONDS_PER_SECOND;
-
+    //coin count tracker
+    let coinCount = 0;
+    console.log("start time:" + startTime);
     setInterval(() => {
         let nowTime = (new Date()).getTime();
+        console.log(`nowTime ${nowTime}`);
         //if current time - start time = 1 second then update
         let elapsed = nowTime - startTime;
-        remainingTime = remainingTime - elapsed;
+        console.log(`elapsed ${elapsed}`);
+        
+        let remainingTime = TOTAL_TIME_MS - elapsed;
+        console.log(`reamainingTime ${remainingTime}`);
+
+        if (isOnTask()) {
+            coinCount += calculateCoinEarned(elapsed, COIN_RATE);
+        }
+
         //update text in html file
-        div.textContent = remainingTime;
+        coinDiv.textContent = coinCount;
+        timerDiv.textContent = remainingTime / MILLISECONDS_PER_SECOND / SECONDS_PER_MINUTE;
     }, 1000);
+
 }
