2d917bf4550a36b4439fae95b54c86428f30aee3
Royl0406 <royliuzy0406@gmail.com>
2022-08-04 17:36:42 -0700

    Refactor isUrlBlocked

--- Common/utilities.js
+++ Common/utilities.js
@@ -4,3 +4,13 @@ export function changePopupLocation (location) {
     {popup: location},
   );
 }
+
+export async function isUrlBlocked(url, blockedList) {
+  for(let i = 0; i < blockedList.length; i++) {
+     if(url === blockedList[i]) {
+        console.log("website found in block list");
+        return true;
+     }
+  }
+  return false;
+}
--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 //Tab Detector
 //background.js
+import {isUrlBlocked} from "./Common/utilities.js";
+
+
 chrome.runtime.onInstalled.addListener(function (e) {
    if(e.reason === chrome.runtime.OnInstalledReason.INSTALL) {
       chrome.storage.local.set({isOnboardingDone: false})
@@ -15,38 +18,26 @@ chrome.runtime.onInstalled.addListener(function (e) {
 
 async function fetchBlockedList() {
    let blockedList = await chrome.storage.local.get(["blocked"]);
-   return blockedList;
+   return blockedList.blocked;
 }
 
-function isUrlDistracting(url) {
-   let blockedList = fetchBlockedList();
-   /*for(let i = 0; i < blockedList.length; i++) {
-      if(url === blockedList[i]) {
-         console.log("website found in block list");
-         return true;
-      }
-   }
-   return false;*/
-
-   if(url === "https://youtube.com") {
-      console.log("distracting site");
-      return true;
-   }
-   return false;
+async function isUrlDistracting(url) {
+   let blockedList = await fetchBlockedList();
+   return isUrlBlocked(url, blockedList);
 }
 
 function showScreenBlocker(tabId) {
    //executes script to change background
    chrome.scripting.executeScript({
       target: { tabId: tabId, allFrames: true },
-      files: ['warning-tab.js']
+      files: ['Screen-Blocker/warning-tab.js']
    });
 }
 
 async function urlChangeHandler(url, tabId) {
    let isDistracted = false;
    let { isDistracted: prevIsDistracted } = await chrome.storage.local.get(['isDistracted']);
-   if (isUrlDistracting(url)) {
+   if (await isUrlDistracting(url)) {
       showScreenBlocker(tabId);
       isDistracted = true;
    }
@@ -78,17 +69,21 @@ let isOnTask = async () => {
 
 function addTabListeners() {
    chrome.tabs.onActivated.addListener(function (activeInfo) {
+      console.log("event listener");
       chrome.tabs.get(activeInfo.tabId, async function (tab) {
          let currentUrl = tab.url;
          let currentTabId = tab.id;
          await urlChangeHandler(currentUrl, currentTabId);
+         console.log("urlChange");
       });
    });
 
    chrome.tabs.onUpdated.addListener(async function(tabId, change, tab) {
       if (tab.active && change.url) {
+         console.log("event listener-update");
          let newUrl = change.url;
          await urlChangeHandler(newUrl, tabId);
+         console.log("urlChange");
       }
    });
 }
