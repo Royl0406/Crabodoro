ae7c3f3750fcaaa9b9eee79766163b4d6b552031
Royl0406 <royliuzy0406@gmail.com>
2022-08-09 19:44:37 -0700

    Install Jest Chrome mockup

---
+++ Common/utilities.test.js
@@ -0,0 +1,5 @@
+import {calcMinutes} from './utilities.js'; 
+
+test('calcMinutes',  () => {
+    expect(calcMinutes(0)).toBe(0);
+})
--- Popup/pomodoro.test.js
+++
@@ -1,5 +0,0 @@
-import {displayMinutes} from './pomodoro.js'; 
-
-test('displayMinutes',  () => {
-    expect(displayMinutes(0)).toBe(0);
-})
