2b0b4eeeb832531050535cc27c1e2e09f72cd0e9
Royl0406 <royliuzy0406@gmail.com>
2022-09-03 15:57:20 -0700

    Added listener for button, (BUG) btn not showing up

--- src/Stats-Screen/stats.js
+++ src/Stats-Screen/stats.js
@@ -2,16 +2,24 @@ import { calcMinutes, calcSeconds } from "../Common/utilities.js";
 import { MAX_COIN } from "../Common/utilities.js";
 import { calcExpEarned } from "./xp-utilities.js";
 import { fetchFocusTime } from "../Common/storage-utilities.js";
+import { changePopupLocation } from '../Common/utilities.js'
 
 document.addEventListener("DOMContentLoaded", async function () {
     const COIN_EARNED = document.getElementById("total-coin");
     const FOCUS_TIME = document.getElementById("focus-time");
     const XP_Earned = document.getElementById("total-xp");
+    const BTN_RETURN = document.getElementById("nav-main");
 
     FOCUS_TIME.innerHTML += displayTime(await fetchFocusTime());
     COIN_EARNED.innerHTML += await fetchEarnedCoin();
     XP_Earned.innerHTML += await calcExpEarned();
+
+    BTN_RETURN.addEventListener("click", () => {
+        var location = "/dist/Game-menus/main.html";
+        changePopupLocation(location);
+    })
 })
+
 
 async function fetchEarnedCoin() {
     let result = await chrome.storage.local.get(["totCoinsEarned", "totalDistractedTime"]);
