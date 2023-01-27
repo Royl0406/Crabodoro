291990932d114974f22fd574b366608d897d9561
Royl0406 <royliuzy0406@gmail.com>
2023-01-26 20:05:13 -0800

    Refactored blocked to blockedList; Created default blockedList for onboarding

---
+++ src/Common/constants.ts
@@ -0,0 +1,8 @@
+export const DEFAULT_BLOCKEDLIST = [
+    "https://www.youtube.com/",
+    "https://www.twitch.tv",
+    "https://www.instagram.com",
+    "https://www.facebook.com",
+    "https://www.twitter.com",
+    "https://www.netflx.com"
+];
