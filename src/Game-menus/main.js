a6ba125b555de332ff504a41f3f247744b853d4d
Royl0406 <royliuzy0406@gmail.com>
2023-02-05 10:46:57 -0800

    Added change num session feature; Refactored button.js to main.js

--- src/Game-menus/main.js
+++ src/Game-menus/main.js
@@ -4,11 +4,11 @@ import { navToPomodoro, navToCrabSpace } from '../Common/utilities.js'
 window.onload = () => {
   var myButton = document.getElementById('btn-start');
   var crabSpaceBtn = document.getElementById("crab-space");
+  var sessionInput = document.getElementById("session-input");
 
   myButton.onclick = async () => {
    
-    //To-do: update with the actual value on the html page later
-    const numSessions = 4;
+    let numSessions = (sessionInput.value < 1 || sessionInput.value > 20) ? 4: sessionInput.value;
     await storeTotSessions(numSessions);
 
     await chrome.runtime.sendMessage("start game");
